361971d0c596b88796514ecda8e3a38c6df443aa
AntonisXT <antonisxt03@gmail.com>
2025-10-02 22:52:14 +0300

    Delete backend directory

--- backend/models/link.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const LinkSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Link', LinkSchema);
